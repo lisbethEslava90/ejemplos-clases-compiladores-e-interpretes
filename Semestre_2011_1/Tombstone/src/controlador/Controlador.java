d9343a4cdba3aad1b32abc5389acd6641dd23c0e
Keyla Abreu <nathy120789@gmail.com>
2011-07-26 16:01:16 -0430

    probando mezcla

--- Semestre_2011_1/Tombstone/src/controlador/Controlador.java
+++ Semestre_2011_1/Tombstone/src/controlador/Controlador.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.util.ListIterator;
@@ -8,18 +12,12 @@ import javax.swing.*;
 import vista.Vista;
 import modelo.Figura;
 import modelo.Modelo;
-import modelo.T;
-import modelo.programa;
-import modelo.maquina;
-import modelo.interprete;
 
 public class Controlador {
 
     private Modelo modelo;
     private Vista vista;
-    private Figura seleccionada, dere, izq, arriba, abajo;
-    private Figura intersectada;
-    Point punto;
+    private Figura seleccionada, agregada;
 
     public Controlador(Modelo modelo, Vista vista) {
         this.modelo = modelo;
@@ -39,30 +37,8 @@ public class Controlador {
         return null;
     }
 
-    private Figura getFiguraEn2(Point posicion, Figura yo) {
-        ListIterator<Figura> it = modelo.getListado().listIterator();
-        while (it.hasNext()) {
-            Figura tmp = it.next();
-            if (!tmp.equals(yo) && tmp.dentroFigura(posicion)) {
-                tmp.setSeleccionada(true);
-                return tmp;
-            }
-        }
-        return null;
-    }
-
-//	public void cambiarPosicion(Figura f, Point p){
-//		f.setPosicion(p);
-//	}
-    public void desplazar(Figura f, Point p) {
-        Point desplazamiento = new Point();
-        desplazamiento.x = p.x - punto.x;
-        desplazamiento.y = p.y - punto.y;
-        punto = new Point();
-        punto.x = p.x;
-        punto.y = p.y;
-        f.desplazar(desplazamiento);
-        f.yasemovieron();
+    public void cambiarPosicion(Figura f, Point p) {
+        f.setPosicion(p);
     }
 
     public Vista getVista() {
@@ -71,6 +47,7 @@ public class Controlador {
 
     public void anyadirFigura(Figura f) {
         modelo.anyadirFigura(f);
+        agregada = f;
     }
 
     public void eliminarFigura(Figura f) {
@@ -81,6 +58,15 @@ public class Controlador {
         return modelo.getFiguraEn(p);
     }
 
+    public void eVmouseClicked(MouseEvent ev) {
+        if (agregada != null) {
+            agregada.setPosicion(ev.getPoint());
+            agregada.visible = true;
+            agregada = null;
+        }
+        vista.repaint();
+    }
+
     public void eVmousePressed(MouseEvent ev) {
         if (SwingUtilities.isLeftMouseButton(ev)) { 			//Click boton izquierdo selecciona figura
             seleccionada = this.getFiguraEn(ev.getPoint());
@@ -89,106 +75,62 @@ public class Controlador {
             this.eliminarFigura(seleccionada);
         } else if (SwingUtilities.isMiddleMouseButton(ev))//click boton medio a�ade figura tipo circulo
         {
-            seleccionada = this.getFiguraEn(ev.getPoint());
-            arriba = seleccionada.vec[0];
-            abajo = seleccionada.vec[1];
-            izq = seleccionada.vec[2];
-            dere = seleccionada.vec[3];
-            for (int i = 0; i < 4; i++) {
-                seleccionada.vec[i] = null;
-            }
-//            System.out.println("dere "+dere);
-//            System.out.println("izq "+izq);
-//            System.out.println("arriba "+arriba);
-//            System.out.println("abajo "+abajo);
-            if (seleccionada instanceof T) {
-                if (dere instanceof programa && dere.vec[2] != null) {
-                    dere.vec[2] = null;
-                }
-                if (izq instanceof programa && izq.vec[3] != null) {
-                    izq.vec[3] = null;
-                }
-                if (dere instanceof T && dere.vec[1] != null) {
-                    dere.vec[1] = null;
-                }
-                if (izq instanceof T && izq.vec[1] != null) {
-                    izq.vec[1] = null;
-                }
-                if (abajo instanceof T && abajo.vec[2] != null) {
-                    abajo.vec[2] = null;
-                }
-                if (abajo instanceof T && abajo.vec[3] != null) {
-                    abajo.vec[3] = null;
-                }
-                if (abajo instanceof maquina && abajo.vec[0] != null) {
-                    abajo.vec[0] = null;
-                }
-                if (arriba instanceof T && arriba.vec[0] != null) {
-                    arriba.vec[0] = null;
-                }
-                if (abajo instanceof interprete && abajo.vec[0] != null) {
-                    abajo.vec[0] = null;
-                }
-            }
-            if (seleccionada instanceof programa) {
-                if (dere instanceof T && dere.vec[2] != null) {
-                    dere.vec[2] = null;
-                }
-                if (izq instanceof T && izq.vec[3] != null) {
-                    izq.vec[3] = null;
-                }
-                if (abajo instanceof maquina && abajo.vec[0] != null) {
-                    abajo.vec[0] = null;
-                }
-            }
-            if (seleccionada instanceof interprete) {
-                if (abajo instanceof maquina && abajo.vec[0] != null) {
-                    abajo.vec[0] = null;
-                }
-                if (arriba instanceof T && arriba.vec[1] != null) {
-                    arriba.vec[1] = null;
-                }
-            }
-            if (seleccionada instanceof maquina) {
-                if (arriba instanceof T && arriba.vec[1] != null) {
-                    arriba.vec[1] = null;
-                }
-                 if (arriba instanceof programa && arriba.vec[1] != null) {
-                    arriba.vec[1] = null;
-                }
-            }
         }
-        punto = ev.getPoint();
         vista.repaint();
     }
 
     public void eVmouseDragged(MouseEvent ev) {
         if (seleccionada != null) {
             //El movimiento puede ser mas fluido recalculando el pto
-            //this.cambiarPosicion(seleccionada, ev.getPoint());
-            this.desplazar(seleccionada, ev.getPoint());
-
+            this.cambiarPosicion(seleccionada, ev.getPoint());
             vista.repaint();
-        }
-        if (seleccionada != null) {
-            intersectada = this.getFiguraEn2(ev.getPoint(), seleccionada);
-            if (intersectada != null) {
-                seleccionada.pegar(intersectada);
-                vista.repaint();
-            }
-
         }
     }
 
     public void eVmouseReleased(MouseEvent ev) {
         vista.repaint();
-        if (intersectada != null && seleccionada != null) {
-            seleccionada.unir(intersectada);
-        }
         if (seleccionada != null) {
             seleccionada.setSeleccionada(false);
             seleccionada = null;
         }
+    }
 
+    public void guardar() {
+        //System.out.println("si guarda");
+        ObjectContainer base = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "BaseDatos");
+        try {
+            modelo.BaseD = JOptionPane.showInputDialog(vista, "Ingrese Nombre del Modelo:");
+            base.store(modelo);
+<<<<<<< HEAD
+            modelo.limpiar(modelo);
+            vista.repaint();
+=======
+>>>>>>> 4f0f8471e7efb538e56cd702ec37a77097327e55
+        } finally {
+            base.close();
+        }
+    }
+
+    public void cargar() {
+        // System.out.println("si carga");
+        ObjectContainer  base1= Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "BaseDatos");
+        try {
+
+<<<<<<< HEAD
+            ObjectSet<Modelo> lista = base1.query(Modelo.class);            
+=======
+            ObjectSet<Modelo> lista = base1.query(Modelo.class);
+            for (Modelo m : lista) {
+                System.out.println("MODELO " + m);
+            }
+>>>>>>> 4f0f8471e7efb538e56cd702ec37a77097327e55
+            Modelo dibu = (Modelo) JOptionPane.showInputDialog(vista, "Mensaje", "Titulo", JOptionPane.INFORMATION_MESSAGE, null, lista.toArray(), lista.get(0));
+            if (dibu != null) {
+                modelo.extraer(dibu);
+            }
+            vista.repaint();
+        } finally {
+            base1.close();
+        }
     }
 }
--- Semestre_2011_1/Tombstone/src/modelo/T.java
+++ Semestre_2011_1/Tombstone/src/modelo/T.java
@@ -1,14 +1,14 @@
+
 package modelo;
 
-import com.sun.corba.se.pept.broker.Broker;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import modelo.Modelo;
 
-public class T extends Figura {
-
+public class T extends Figura
+{
     private int ancho;
+<<<<<<< HEAD
     public String de = "", para = "", en = "";
 
     public T() {
@@ -33,11 +33,13 @@ public class T extends Figura {
 
     public boolean dentroFigura(Point p) {
         int difX = Math.abs(p.x - (posicion.x + (ancho / 2)));
-        int difY = p.y - (posicion.y);
-        return ((difX < ancho / 2) && (difY > 0 && difY < 80));
+        int difY = Math.abs(p.y - (posicion.y + (ancho / 2)));
+        return ((difX < ancho / 2) && (difY < ancho / 2));
     }
 
     public void dibujar(Graphics g) {
+            if(visible){
+
         g.setColor(Color.BLUE);
         g.fillRect(this.getX(), this.getY(), this.getAncho(), this.getAncho() / 4 - 10);
         g.fillRect(this.getX() + 60, this.getY(), this.getAncho() - 120, this.getAncho() - 120);
@@ -50,6 +52,7 @@ public class T extends Figura {
             g.drawRect(this.getX() + 7, this.getY() + 7, this.getAncho() - 20, this.getAncho() / 4 - 20);
             g.drawRect(this.getX() + 67, this.getY() + 7, this.getAncho() - 135, this.getAncho() - 135);
         }
+            }
     }
 
     @Override
@@ -74,7 +77,16 @@ public class T extends Figura {
             p.y -= 80;
             this.setPosicion(p);
         }
-
+        if (intersectada instanceof programa
+                && this.vec[3] == null
+                && intersectada.vec[2] == null
+                && this.de.equals(((programa) intersectada).para)) //programa
+        {
+            Point p = new Point(intersectada.posicion);
+            p.x += 60;
+            p.y += 60;
+            this.setPosicion(p);
+        }
         if (intersectada instanceof T
                 && this.vec[1] == null
                 && intersectada.vec[2] == null
@@ -93,74 +105,59 @@ public class T extends Figura {
             p.x += 120;
             p.y -= 40;
             this.setPosicion(p);
-        }
-        if (intersectada instanceof programa
-                && this.vec[3] == null
-                && intersectada.vec[2] == null
-                && this.para.equals(((programa) intersectada).para)) //programa lado izq
-        {
-            Point p = new Point(intersectada.posicion);
-            p.x -= 180;
-            p.y += 60;
-            this.setPosicion(p);
         } else if (intersectada instanceof programa
                 && this.vec[2] == null
                 && intersectada.vec[3] == null
                 && this.de.equals(((programa) intersectada).para)) //programa
         {
             Point p = new Point(intersectada.posicion);
-            p.x += 60;
+            p.x -= 190;
             p.y += 60;
             this.setPosicion(p);
         }
     }
+=======
+    private String de="", para="", en="";
+	public T(Point posicion, int ancho, String de, String para, String en){
+		this.posicion=posicion;
+		this.ancho=ancho;
+		this.seleccionada=false;
+                this.de = de;
+                this.para = para;
+                this.en = en;
+	}
 
-    @Override
-    public void unir(Figura intersectada) {
-        if (intersectada instanceof interprete
-                && this.vec[1] == null
-                && intersectada.vec[0] == null
-                && this.en.equals(((interprete) intersectada).de)) {
-            this.vec[1] = intersectada;
-            intersectada.vec[0] = this;
-        }
-        if (intersectada instanceof maquina
-                && this.vec[1] == null
-                && intersectada.vec[0] == null
-                && this.en.equals(((maquina) intersectada).de)) //maquina
-        {
-            this.vec[1] = intersectada;
-            intersectada.vec[0] = this;
-        }
-        if (intersectada instanceof T
-                && this.vec[1] == null
-                && intersectada.vec[2] == null
-                && this.en.equals(((T) intersectada).de))// tombstom izquierda
-        {
-            this.vec[1] = intersectada;
-            intersectada.vec[2] = this;
-        } else if (intersectada instanceof T
-                && this.vec[1] == null
-                && intersectada.vec[3] == null
-                && this.en.equals(((T) intersectada).para)) //tombston derecho
-        {
-            this.vec[1] = intersectada;
-            intersectada.vec[3] = this;
-        }
-        if (intersectada instanceof programa
-                && this.vec[3] == null
-                && intersectada.vec[2] == null
-                && this.para.equals(((programa) intersectada).para)) //programa
-        {
-            this.vec[3] = intersectada;
-            intersectada.vec[2] = this;
+	public void setAncho(int ancho){
+		this.ancho=ancho;
+	}
+	public int getAncho(){
+		return ancho;
+	}
+	
+	public boolean dentroFigura(Point p) {
+		int difX=Math.abs(p.x-(posicion.x+(ancho/2)));
+		int difY=(p.y-(posicion.y));
+		return ( (difX<ancho/2) && (difY>0 && difY<80) );
+	}
+	
+	public void dibujar(Graphics g)
+	{
+            if(visible){
 
-        } else if (intersectada instanceof programa
-                && this.vec[2] == null
-                && intersectada.vec[3] == null
-                && this.de.equals(((programa) intersectada).para)) {
-            this.vec[2] = intersectada;
-            intersectada.vec[3] = this;
-        }
-    }
+		g.setColor(Color.BLUE);
+		g.fillRect(this.getX(), this.getY(), this.getAncho(), this.getAncho()/4-10);
+                g.fillRect(this.getX()+60, this.getY(), this.getAncho()-120, this.getAncho()-120);
+                g.setColor(Color.BLACK);
+                g.drawString(de, this.getX()+20, this.getY()+25);
+                g.drawString(para, this.getX()+150, this.getY()+25);
+                g.drawString(en, this.getX()+85, this.getY()+70);
+		if(this.getSeleccionada()){
+			g.setColor(Color.RED);
+			g.drawRect(this.getX()+7, this.getY()+7, this.getAncho()-20, this.getAncho()/4-20);
+                        g.drawRect(this.getX()+67, this.getY()+7, this.getAncho()-135, this.getAncho()-135);
+		}
+            }
+	}
+>>>>>>> 4f0f8471e7efb538e56cd702ec37a77097327e55
+
 }
